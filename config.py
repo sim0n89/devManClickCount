e0cfba48933449c976dc4098af34b0561a92ddb9
Виталий Ильин <sima26ru@gmail.com>
2023-07-08 14:15:22 +0300

    удаляем config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-from os.path import join, dirname
-
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-BITLY_TOKEN = os.environ.get("TOKEN")
